95ea0779adaa092e25651ea60aa60aaab5633b6d
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-09-07 12:07:43 -0300

    Add files via upload

---
+++ ListaPOO_Exe3/ListaPOO_Exe3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOO_Exe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadrad = new Quadrado();
+            Console.WriteLine("digite o valor da diagonal: ");
+            quadrad.setAresta(int.Parse(Console.ReadLine()));
+            quadrad.calcularArea();
+            Console.WriteLine("A area do quadrado é:{0}", quadrad.getResult());
+        }
+    }
+}
---
+++ ListaPOO_Exe3/ListaPOO_Exe3/Quadrado.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOO_Exe2
+{
+    internal class Quadrado
+    {
+        int diag;
+
+        int result;
+
+        public Quadrado()
+        {
+            this.diag = 0;
+            this.result = 0;
+
+        }
+
+        public Quadrado(int aresta)
+        {
+            this.diag = aresta;
+
+        }
+
+        public void setAresta(int aresta)
+        {
+            this.diag = aresta;
+        }
+
+        public int getAresta()
+        {
+            return this.diag;
+        }
+
+
+
+        public int getResult()
+        {
+            return this.result;
+        }
+
+
+
+        public void calcularArea()
+        {
+            this.result = (this.diag * this.diag)/2;
+        }
+    }
+}
