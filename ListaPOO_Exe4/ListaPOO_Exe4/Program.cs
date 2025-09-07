544bd9a56bb2a94b48cd3e532c7e9b6a9da12063
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-09-07 12:14:41 -0300

    Add files via upload

---
+++ ListaPOO_Exe4/ListaPOO_Exe4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOO_Exe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triang = new Triangulo();
+            Console.WriteLine("digite o valor da base: ");
+            triang.setBase(int.Parse(Console.ReadLine()));
+            Console.WriteLine("digite o valor da altura: ");
+            triang.setAltura(int.Parse(Console.ReadLine()));
+            triang.calcularArea();
+            Console.WriteLine("A area do quadrado é:{0}", triang.getResult());
+        }
+    }
+}
---
+++ ListaPOO_Exe4/ListaPOO_Exe4/Triangulo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOO_Exe4
+{
+    internal class Triangulo
+    {
+        int b;
+        int a;
+        int result;
+
+        public Triangulo()
+        {
+            this.b = 0;
+            this.a = 0;
+            this.result = 0;
+
+        }
+
+        public Triangulo(int b)
+        {
+            this.b = b;
+
+        }
+
+        public void setBase(int b)
+        {
+            this.b = b;
+        }
+
+        public int getBase()
+        {
+            return this.b;
+        }
+        
+        public void setAltura(int a)
+        {
+            this.a = a;
+        }
+        
+        public int getAltura(int a)
+        {
+            return this.a;
+        }
+
+        public int getResult()
+        {
+            return this.result;
+        }
+
+
+
+        public void calcularArea()
+        {
+            this.result = this.b * this.a /2;
+        }
+    }
+}
