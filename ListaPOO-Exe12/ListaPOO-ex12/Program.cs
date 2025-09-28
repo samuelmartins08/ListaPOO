240dfe9f9037d72450d13f42fd45d03e22a03b60
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-09-28 09:04:06 -0300

    Add files via upload

---
+++ ListaPOO-Exe12/ListaPOO-ex12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOO_ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do lado A:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do lado B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do lado C:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            Triangulo triangulo = new Triangulo(a, b, c);
+
+            if (triangulo.FormaTriangulo())
+            {
+                Console.WriteLine("Os lados formam um triângulo.");
+                Console.WriteLine("Classificação:{0}", triangulo.Classificar());
+            }
+            else
+            {
+                Console.WriteLine("Os valores informados NÃO formam um triângulo.");
+            }
+        }
+    }
+}
---
+++ ListaPOO-Exe13/ListaPOO-ex13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOO_ex13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do lado A:");
+            double a = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do lado B:");
+            double b = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do lado C:");
+            double c = Double.Parse(Console.ReadLine());
+
+            // Cria o objeto TrianguloRetangulo
+            TrianguloRetangulo triangulo = new TrianguloRetangulo(a, b, c);
+
+            // Verifica e exibe o resultado
+            if (triangulo.FormaTriangulo())
+            {
+                if (triangulo.EhRetangulo())
+                {
+                    Console.WriteLine("Os valores formam um triângulo retângulo.");
+                }
+                else
+                {
+                    Console.WriteLine("Os valores formam um triângulo, mas NÃO é retângulo.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Os valores NÃO formam um triângulo.");
+            }
+        }
+    }
+}
